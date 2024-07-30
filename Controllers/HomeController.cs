5408f7a189db323e457de9fc8f32d105d3ca484f
JordanPierre91630 <152972656+JordanPierre91630@users.noreply.github.com>
2024-07-30 11:16:47 +0200

    Add files via upload

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ViewEngines;
+using System.Reflection;
+using System.Security.Cryptography.X509Certificates;
+using System.Text.RegularExpressions;
+using TPLOCAL1.Models;
+
+//Subject is find at the root of the project and the logo in the wwwroot/ressources folders of the solution
+//--------------------------------------------------------------------------------------
+//Careful, the MVC model is a so-called convention model instead of configuration,
+//The controller must imperatively be name with "Controller" at the end !!!
+namespace TPLOCAL1.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        //methode "naturally" call by router
+        public ActionResult Index(string id)
+        {
+            if (string.IsNullOrWhiteSpace(id))
+                //retourn to the Index view (see routing in Program.cs)
+                return View();
+            else
+            {
+                //Call different pages, according to the id pass as parameter
+                switch (id)
+                {
+                    case "OpinionList":
+                        List<Opinion> opinionlist = new OpinionList().GetAvis(@"XlmFile/DataAvis.xml");
+                        ViewBag.opinionlist = opinionlist;
+                        return View(id);
+                        //TODO : code reading of the xml files provide
+                        
+                    case "Form":
+                        //TODO : call the Form view with data model empty
+                        return View(id);
+                    
+                    default:
+                        //retourn to the Index view (see routing in Program.cs)
+                        return View();
+                }
+            }
+        }
+
+
+
+             
+        public ActionResult ValidationFormulaire(FormModel FormModel)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View("/Views/Shared/Form.cshtml");
+
+            }
+            return View(FormModel);
+        }
+
+
+    }
+
+
+
+
+}
+            
+ 
---
+++ Models/FormModel.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+
+namespace TPLOCAL1.Models
+{
+    public class FormModel
+    {
+        [Required]
+        [Display(Name = "Nom")]
+        public  string  Nom {  get; set; }
+        [Required]
+        [Display(Name = "Prenom")]
+        public  string  Prenom { get; set; }
+        [Required]
+        [Display(Name = "Sexe")]
+        public  string  Sexe { get; set; }
+        [Required]
+        [Display(Name = "Adresse")]
+        public  string  Adresse { get; set; }
+        [Required]
+        [RegularExpression(@"^(\b\d{5}\b)$")]
+        [Display(Name = "Code Postal")]
+        public  string  Zipcode { get; set; }
+        [Required]
+        [Display(Name = "Ville")]
+        public  string  Ville { get; set; }
+        [Required]
+        [Display(Name = "Adresse Mail")]
+        public  string  Email { get; set; }
+        [Required]
+        [Display(Name = "Date de début de formation")]
+        public  DateTime Debut { get; set; }
+        [Required]
+        [Display(Name = "Type de formation")]
+        public  string  Formation { get; set; }
+        [Required]
+        [Display(Name = "Avis sur la formation COBOL")]
+        public  string  Avis1 { get; set; }
+        [Required]
+        [Display(Name = "Avis sur la formation C#")]
+        public  string  Avis2 { get; set; }
+
+
+    }
+}
